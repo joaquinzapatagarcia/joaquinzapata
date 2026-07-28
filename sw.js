7e5a976c04888a3d5287addc0d0e7b7c8a641c44
joaquinzapatagarcia <joaquinzapata@gmail.com>
2026-07-28 11:26:12 +0200

    Renueva caché tras integrar informes JZG

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jzg-situacion-v4';
+const CACHE='jzg-situacion-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
